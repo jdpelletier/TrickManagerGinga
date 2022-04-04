e17caf2de42d63b5f90f9c9aeead0d021b9bc401
jdpelletier <john.pelletier@ymail.com>
2022-04-03 15:22:47 -1000

    Update TMnewUI.py

--- TMnewUI.py
+++ TMnewUI.py
@@ -145,7 +145,7 @@ class FitsViewer(QtGui.QMainWindow):
         viewer_hbox = QtGui.QHBoxLayout()
         viewer_hbox.setObjectName("viewer_hbox")
         w = fi.get_widget()
-        w.setMinimumSize(QtCore.QSize(240, 260))
+        w.setMinimumSize(QtCore.QSize(230, 260))
         viewer_hbox.addWidget(w)
         hw = QtGui.QWidget()
         hw.setLayout(viewer_hbox)
