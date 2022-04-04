cb32cc433f799ff3800e7adf0698d6d1b8b32d80
jdpelletier <john.pelletier@ymail.com>
2022-04-03 15:25:42 -1000

    Update TMnewUI.py

--- TMnewUI.py
+++ TMnewUI.py
@@ -411,7 +411,7 @@ class FitsViewer(QtGui.QMainWindow):
         image = self.pixels_to_image(pix)
         self.img.load_data(image)
         self.fitsimage.set_image(self.img)
-        self.resize(250, 300)
+        # self.resize(250, 300)
 
     def pixels_to_image(self, pix):
         lst = str(pix).strip().replace(':', '').split()
