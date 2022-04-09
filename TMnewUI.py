2bc6c0d6dd36fdc2a9b761839d6ef1c0e4c17722
jdpelletier <john.pelletier@ymail.com>
2022-04-09 03:10:22 -1000

    Update TMnewUI.py

--- TMnewUI.py
+++ TMnewUI.py
@@ -424,6 +424,7 @@ class FitsViewer(QtGui.QMainWindow):
         image = self.pixels_to_image(pix, ff, bg)
         self.img.load_data(image)
         self.fitsimage.set_image(self.img)
+        self.fitsimage.set_pan(8, 8, coord='data', no_reset=False)
         self.resize(240, 300)
 
     def pixels_to_image(self, pix, ff, bg):
