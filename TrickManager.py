650e562db932376c338e2f5fb265bedd22a5156a
jdpelletier <john.pelletier@ymail.com>
2022-04-14 12:19:53 -1000

    Update TrickManager.py

--- TrickManager.py
+++ TrickManager.py
@@ -524,8 +524,8 @@ class FitsViewer(QtGui.QMainWindow):
         pixelvalues = np.array(lst[1::2],dtype=float) # take every second value, since the first value is the pixel number
         pixelvalues_ff = np.array(lst_ff[1::2],dtype=float)
         pixelvalues_bg = np.array(lst_bg[1::2],dtype=float)
-        roix = int(self.trickxsize.read())
-        roiy = int(self.trickysize.read())
+        roix = float(self.trickxsize.read())
+        roiy = float(self.trickysize.read())
         reduction = 16/roix
         dims = int(np.sqrt(pixelvalues.shape))
         image = np.reshape(pixelvalues,(dims/reduction,dims/reduction))
